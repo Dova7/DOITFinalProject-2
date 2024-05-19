b19e7fa2b57830bbfa99f9abb9e3e42cf5614736
George Gujarelidze <gujarelidze@gmail.com>
2024-05-19 23:37:19 +0400

    install: EntityFramework

---
+++ ForumProject.Data/ApplicationDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ForumProject.Data
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}

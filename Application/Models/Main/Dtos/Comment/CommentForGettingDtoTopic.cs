94aef501faa0e617d6fbf49ed9d6187529a4822a
George Gujarelidze <gujarelidze@gmail.com>
2024-06-14 03:33:46 +0400

    add: comment count

--- Application/Models/Main/Dtos/Comment/CommentForGettingDtoTopic.cs
+++ Application/Models/Main/Dtos/Comment/CommentForGettingDtoTopic.cs
@@ -4,6 +4,6 @@
     {
         public string Body { get; set; } = null!;
         public DateTime PostDate { get; set; }
-        public string UserName { get; set; } = null!;
+        public string DisplayName { get; set; } = null!;
     }
 }
